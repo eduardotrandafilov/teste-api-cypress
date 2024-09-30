7d93fd7800c18ce126c1e5f52a308684869cf20a
Eduardo <eduardo.trandafilov@gmail.com>
2024-09-30 17:05:24 -0300

    refatoração do codigo

--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -1,6 +1,15 @@
 /// <reference types="cypress"/>
 
 describe('Teste de API em produtos', () => {
+
+    let token
+    
+    beforeEach(() => {
+        cy.token('fulano@qa.com','teste').then(tkn =>{
+            token = tkn
+        })
+    });
+
     it('Listar produtos - GET', () => {
         cy.request({
             method: 'GET',
@@ -11,22 +20,22 @@ describe('Teste de API em produtos', () => {
         })
     });
 
-    it('Cadastrar produto - POST', () => {
-        //TODO: criar token dinamicamente
-        let token = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImZ1bGFub0BxYS5jb20iLCJwYXNzd29yZCI6InRlc3RlIiwiaWF0IjoxNzI3NzA5MzU4LCJleHAiOjE3Mjc3MDk5NTh9.Kl60ItYYW2qNja97HUfOrjSZjNdKmsPVh8wFvHxKKgo"
-        cy.request({
-            method: 'POST',
-            url: 'produtos',
-            headers:{authorization: token},
-            body:{
-                //TODO: criar produto dinamicamente
-                "nome": "Cabo usb - 001",
-                "preco": 15,
-                "descricao": "Cabo usb tipo micro-usb",
-                "quantidade": 100
-            }
-        }).should(response=>{
+    it('Cadastrar produto com sucesso- POST', () => {
+        let produto = 'Produto EBAC ' + Math.floor(Math.random() * 100000000000)
+        cy.cadastrarProduto(token,produto,10,'Cabo USB C',100)
+        .should(response=>{
             expect(response.body.message).equal('Cadastro realizado com sucesso')
+            expect(response.status).equal(201)
+
         })
     });
-});
+
+    it('Deve validar mensagem de produto cadastrado anteriormente', () => {
+        cy.cadastrarProduto(token,'Cabo USB 001',10,'Cabo USB C',100)
+        .should(response=>{
+            expect(response.body.message).equal('Já existe produto com esse nome')
+            expect(response.status).equal(400)
+
+        })
+    });
+}); 
